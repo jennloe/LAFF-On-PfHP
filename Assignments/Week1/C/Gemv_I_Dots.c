70855f3f0cf91455cccfed271cce8c94e3014c49
Jennifer Loe <jennifer_loe@baylor.edu>
2020-01-31 15:41:14 -0700

    Finished exercises thru section 1.4

--- Assignments/Week1/C/Gemv_I_Dots.c
+++ Assignments/Week1/C/Gemv_I_Dots.c
@@ -3,10 +3,17 @@
 #define psi( i )  y[ (i)*incy ]         // map psi( i )  to array y
 
 void Dots( int, const double *, int, const double *, int, double * );
+// size of vectors to dot product,
+// location of first vector
+// increment of first vector
+// location of 2nd vector
+// increment of 2nd vector
+// location for answer
 
+// y = Ax+y
 void MyGemv( int m, int n, double *A, int ldA,
            double *x, int incx, double *y, int incy )
 {
   for ( int i=0; i<m; i++ )
-    Dots(  ,     ,     ,     ,       ,      );
+    Dots( n,&(alpha(i,0)), ldA, x, incx, &(psi(i)));
 }
